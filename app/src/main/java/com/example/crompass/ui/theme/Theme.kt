ae2a60bacb6cc7bf9eb98a2292350591c564516e
Josip Hudolin <97687545+JosipHudolin@users.noreply.github.com>
2025-04-07 16:20:27 +0200

    created home screen

---
+++ app/src/main/java/com/example/crompass/ui/theme/Theme.kt
@@ -0,0 +1,53 @@
+package com.example.crompass.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFFE53935),
+    onPrimary = Color.White,
+    secondary = Color(0xFF1976D2),
+    tertiary = Color(0xFF00ACC1),
+    background = Color(0xFFFAFAFA),
+    surface = Color(0xFFFFFFFF),
+    onSurface = Color.Black
+)
+
+@Composable
+fun CROmpassTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
